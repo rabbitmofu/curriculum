6cb4bb55619a69a2c391e7a453b489bcc79425e2
rabbitmofu <danron1205@gmail.com>
2023-08-22 17:53:55 +0900

    1章課題提出

---
+++ javastudy/1-1/src/task1/Task1_1.java
@@ -0,0 +1,39 @@
+package task1;
+
+public class Task1_1 {
+
+	public static void main(String[] args) {
+		
+		//①
+		int hogeInt = 50;
+		
+		
+		//②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化	
+		
+		String hogeString = " テスト ";
+		
+
+	
+		//③下記の処理について、何をしているのかコメントを記入してください
+        // double型 の hogeDouble という名前の変数宣言と、値「0.8」で初期化
+		 double hogeDouble = 0.8;
+		 
+		 // ④変数 hogeInt を値「100」で上書き
+		 hogeInt = 100 ;
+		 System.out.println(hogeInt);
+		 
+		 // ⑤変数 hogeString を値「良くできました！」で上書き
+		 
+		 hogeString = " 良くできました！ ";
+		 System.out.println(hogeString);
+		 
+		 // ⑦下記の処理について、何をしているのかコメントを記入してください
+	     // ()の中、つまり「hogeString」を表示する処理
+		 //System.out.println(hogeString);
+	      
+		 //⑧変数 hogeDouble の値を表示
+	      System.out.println(hogeDouble);
+		 
+	}
+
+}
